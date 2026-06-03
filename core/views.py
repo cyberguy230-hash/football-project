52897ad2e2be963cf4b314f5a9a140dceeacfd3d
Nitro <cyberguy230@gmail.com>
2026-06-03 18:52:57 +0545

    temp admin creator

--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Football
+from django.contrib.auth.models import User
 # Create your views here.
 def soccer(request):
     if request.method == "POST":
@@ -26,3 +27,13 @@ def submit(request):
         return render(request, "submit.html")
     else:
         return render(request, "submit.html")
+    
+def create_admin(request):
+    if not User.objects.filter(username="admin").exists():
+        User.objects.create_superuser(
+            username="superadmin",
+            email="admin@test.com",
+            password="superadmin@2064"
+        )
+        return HttpResponse("Admin created")
+    return HttpResponse("Already exists")
